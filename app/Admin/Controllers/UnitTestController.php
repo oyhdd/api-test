1940dd1370a5357046f911142a3919dc8d5f2866
日月丛林 <1040089624@qq.com>
2021-03-26 16:50:39 +0800

    单元测试

--- app/Admin/Controllers/UnitTestController.php
+++ app/Admin/Controllers/UnitTestController.php
@@ -2,10 +2,15 @@
 
 namespace App\Admin\Controllers;
 
-use App\Admin\Repositories\UnitTest;
+use App\Admin\Repositories\{UnitTest, Api};
+use App\Models\ApiModel;
 use Dcat\Admin\Form;
 use Dcat\Admin\Grid;
 use Dcat\Admin\Show;
+use Dcat\Admin\Layout\Column;
+use App\Models\BaseModel;
+use Dcat\Admin\Layout\Content;
+use Dcat\Admin\Layout\Row;
 
 class UnitTestController extends AdminController
 {
@@ -57,6 +62,28 @@ class UnitTestController extends AdminController
     }
 
     /**
+     * Create interface.
+     *
+     * @param Content $content
+     *
+     * @return Content
+     */
+    public function create(Content $content)
+    {
+        return $content
+            ->title($this->title())
+            ->description($this->description()['create'] ?? trans('admin.create'))
+            ->row(function (Row $row) {
+                $row->column(5, function (Column $column) {
+                    $column->append($this->apiDetail(1));
+                });
+                $row->column(7, function (Column $column) {
+                    $column->append($this->form());
+                });
+            });
+    }
+
+    /**
      * Make a form builder.
      *
      * @return Form
@@ -70,10 +97,46 @@ class UnitTestController extends AdminController
             $form->text('name');
             $form->text('header');
             $form->text('body');
-            $form->text('status');
-        
             $form->display('created_at');
             $form->display('updated_at');
+        });
+    }
+
+    /**
+     * Make a Api show builder.
+     *
+     * @param mixed $id
+     *
+     * @return Show
+     */
+    protected function apiDetail($api_id = 0)
+    {
+        return Show::make($api_id, Api::with(['project']), function (Show $show) {
+            $show->field('project.name', '项目');
+            $show->field('name');
+            $show->field('url')->label('info');
+            $show->field('alarm_enable')->as(function ($alarm_enable) {
+                return BaseModel::$label_yes_or_no[$alarm_enable] ?? '';
+            });
+            $show->field('method')->label('success');
+            $show->field('desc')->textarea();
+            $show->field('request_example')->textarea();
+            $show->field('response_example')->textarea();
+            $show->field('response_desc')->textarea();
+
+            $show->panel()->title('接口信息')->tools(function ($tools) {
+                $tools->disableEdit()->disableList()->disableDelete();
+            });
+
+            $show->setWidth(9);
+
+            \Dcat\Admin\Admin::script("$(\"select[name='api_id']\").change(function () {
+                $.pjax.reload({
+                    url: '/admin/unit_test/create?api_id='+$(\"select[name='api_id']\").val(),
+                    container: '#api_refresh'
+                });
+            });");
+            // return "<div id='api_refresh'>" . $show->render() . "</div>";
         });
     }
 }
--- resources/lang/zh_CN/unit-test.php
+++ resources/lang/zh_CN/unit-test.php
@@ -5,11 +5,16 @@ return [
         'unit-test' => 'UnitTest',
     ],
     'fields' => [
-        'project_id' => '项目id',
-        'api_id' => '接口id',
+        'project_id' => '项目',
+        'api_id' => '接口',
         'name' => '用例名称',
         'header' => 'header参数，json格式',
         'body' => 'body参数，json格式',
+        'url' => '接口地址',
+        'desc' => '接口描述',
+        'request_example' => '请求示例',
+        'response_example' => '返回示例',
+        'response_desc' => '返回值说明',
     ],
     'options' => [
     ],
